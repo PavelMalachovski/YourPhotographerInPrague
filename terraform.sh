a092a218fac04c9ad3d0a997d61c40afca7de717
Pavel Malakhouski <p.malakhouski@gmail.com>
2023-01-15 18:10:12 +0100

    update pipeline10

--- terraform.sh
+++ terraform.sh
@@ -2,22 +2,31 @@
 set -e
 
 #=============================================================================
-#=============================================================================
 # Bash script to run terraform via Github Actions
 #=============================================================================
-#=============================================================================
 
+#=============================================================================
+# Variables
+PROJECT_DIR="/home/runner/work/YourPhotographerInPrague/YourPhotographerInPrague"
 #=============================================================================
 echo "Install terraform"
 if ! rpm -qa | grep -i "terra"
 then
-  apt install -y apt-utils
-  apt-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-  apt install -y terraform
+    sudo apt update
+    sudo apt install wget unzip
+    TER_VER=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
+    wget https://releases.hashicorp.com/terraform/${TER_VER}/terraform_${TER_VER}_linux_amd64.zip
+    unzip terraform_${TER_VER}_linux_amd64.zip
+    sudo mv terraform /usr/local/bin/
+    which terraform
+    terraform version
+
+#   apt-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+#   apt install -y terraform
 fi
 #=============================================================================
 
-cd terraform/YPIP/development
+cd ${PROJECT_DIR}/terraform/YPIP/development
 terraform init && terraform plan #&& terraform apply
   #-var-file=secret.tfvars \
  #-var-file=secret.tfvars 
