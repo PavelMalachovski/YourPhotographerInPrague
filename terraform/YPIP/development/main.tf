55c8aae23773afc7654be418313f475dd9142ab5
Pavel Malakhouski <p.malakhouski@gmail.com>
2023-01-15 18:34:13 +0100

    update terraform files2

--- terraform/YPIP/development/main.tf
+++ terraform/YPIP/development/main.tf
@@ -2,7 +2,11 @@ provider "aws" {
     region = "eu-central-1"
 }
 
-module "aws_infrastructure_development" {
+module "aws_security_group" {
+    source = "../../modules/aws_security_group"
+    allow_ports = ["22","80","443","8080","1541","9092"]
+}
+module "aws_infrastructure" {
     source = "../../modules/aws_infrastructure"
     master_instance_type = "t2.medium"
     worker_instance_type = "t2.micro"
@@ -11,9 +15,4 @@ module "aws_infrastructure_development" {
 module "aws_s3bucket" {
     source = "../../modules/aws_s3bucket"
     acl = "public-read-write"
-}
-
-module "aws_security_group" {
-    source = "../../modules/aws_security_group"
-    allow_ports = ["22","80","443","8080","1541","9092"]
 }
