f95083a79d9ba9893a0039fff31354bbd989374a
Pavel Malakhouski <p.malakhouski@gmail.com>
2023-01-17 17:51:02 +0100

    "test"

--- main.tf
+++ main.tf
@@ -1,48 +1,58 @@
-provider "github" {
-    token = var.github_token
-}
+provider "local" {}
 
 #=============================================================================
 # Git push
 #=============================================================================
-# locals {
-#   timestamp = "${timestamp()}"
-#   timestamp_sanitized = "${replace("${local.timestamp}", "/[-| |T|Z|:]/", "")}"
+locals {
+  timestamp = "${timestamp()}"
+  timestamp_sanitized = "${replace("${local.timestamp}", "/[-| |T|Z|:]/", "")}"
 
-# }
+}
 
-# resource "null_resource" "cmd" {
-#     provisioner "local-exec" {
-#         command = "echo Terraform apply started: ${local.timestamp_sanitized} >> project-run.log"
-#     }
-#     triggers = {
-#         always_run = "${timestamp()}"
-#     }
-# }
+resource "null_resource" "cmd" {
+    provisioner "local-exec" {
+        command = "echo Terraform apply started: ${local.timestamp_sanitized} >> terraform.log"
+    }
+    triggers = {
+        always_run = "${timestamp()}"
+    }
+}
 
-# resource "null_resource" "git_add" {
-#     provisioner "local-exec" {
-#         command = "git add ."
-#     }
-#     triggers = {
-#         always_run = "${timestamp()}"
-#     }
-# }
+resource "null_resource" "git_add" {
+    provisioner "local-exec" {
+        command = "git add ."
+    }
+    triggers = {
+        always_run = "${timestamp()}"
+    }
 
-# resource "null_resource" "git_commit" {
-#     provisioner "local-exec" {
-#         command = "git commit -m 'start project ${local.timestamp_sanitized}'"
-#     }
-#     triggers = {
-#         always_run = "${timestamp()}"
-#     }
-# }
+    depends_on = [
+      null_resource.cmd
+    ]
+}
 
-# resource "null_resource" "git_push" {
-#     provisioner "local-exec" {
-#         command = "git push"
-#     }
-#     triggers = {
-#         always_run = "${timestamp()}"
-#     }
-# }
+resource "null_resource" "git_commit" {
+    provisioner "local-exec" {
+        command = "git commit -m \"test\""
+    }
+    triggers = {
+        always_run = "${timestamp()}"
+    }
+
+    depends_on = [
+      null_resource.git_add
+    ]
+}
+
+resource "null_resource" "git_push" {
+    provisioner "local-exec" {
+        command = "git push"
+    }
+    triggers = {
+        always_run = "${timestamp()}"
+    }
+
+    depends_on = [
+      null_resource.git_commit
+    ]
+}
