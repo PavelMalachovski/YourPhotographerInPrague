d3662cc7699388df473527600e46c939da301694
Pavel Malakhouski <pavel.malakhouski@gmail.com>
2023-01-17 21:27:29 +0100

    add vpc

--- terraform/modules/aws_network/variables.tf
+++ terraform/modules/aws_network/variables.tf
@@ -2,3 +2,21 @@ variable "aws_region" {
     description = "The AWS region to use to create resources"
     default = "eu-central-1"
 }
+
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+variable "env" {
+    default = "dev"
+}
+
+variable "public_subnet_cidrs" {
+    default = [
+        "10.0.1.0/24",
+    ]
+}
+
+variable "private_subnet_cidrs" {
+    default = []
+}
