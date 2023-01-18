0b10ac6ca9c5bfc25aaafa1f268c17aca7bfd104
Pavel Malakhouski <p.malakhouski@gmail.com>
2023-01-18 19:03:37 +0100

    update terraform files

--- terraform/YPIP/development/output.tf
+++ terraform/YPIP/development/output.tf
@@ -2,3 +2,13 @@ output "private_key" {
   value     = tls_private_key.example.private_key_pem
   sensitive = true
 }
+
+output "my_k8s_cluster_master_public_ip" {
+  value = aws_instance.my_k8s_cluster_master.public_ip
+}
+output "my_k8s_cluster_worker1_public_ip" {
+  value = aws_instance.my_k8s_cluster_worker1.public_ip
+}
+output "my_k8s_cluster_worker2_public_ip" {
+  value = aws_instance.my_k8s_cluster_worker2.public_ip
+}
