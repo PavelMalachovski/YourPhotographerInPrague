59b95641385bec7747cb1a50c3c7838429e48883
Pavel Malakhouski <p.malakhouski@gmail.com>
2023-01-18 21:53:27 +0100

    "terrastart"

---
+++ terraform/YPIP/development/variables.tf
@@ -0,0 +1,60 @@
+variable "region" {
+  description = "Please enter AWS region to deploy server"
+  default = "eu-central-1"
+  type = string
+}
+
+variable "owners" {
+    default = ["amazon"]
+    type = list
+}
+
+variable "latest_amazon_linux" {
+    type = list
+    default = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+}
+
+variable "master_instance_type" {
+    description = "Enter Instance TYpe"
+    default = "t2.small"
+    type = string
+}
+
+variable "worker_instance_type" {
+    description = "Enter Instance TYpe"
+    default = "t2.micro"
+    type = string
+}
+
+variable "detailed_monitoring" {
+    default = false
+    type = bool
+}
+
+variable "environment" {
+    description = "environment type"
+    default = "development"
+}
+
+variable "common_tag" {
+    description = "Common tags to apply to all resources"
+    type = map
+    default = {
+        Owner = "Pavel Malachovski"
+        Project = "TeachMeSkills Diploma"
+    }
+}
+
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+    default = [
+        "10.0.1.0/24"
+    ]
+}
+
+variable "key_name" {
+    default = "key"
+}
