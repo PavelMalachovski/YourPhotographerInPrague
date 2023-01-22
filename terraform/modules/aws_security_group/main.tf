702d9787239b417ab9f3e40389a7cc616724a576
Pavel Malakhouski <p.malakhouski@gmail.com>
2023-01-22 21:36:45 +0100

    "terrastart"

--- terraform/modules/aws_security_group/main.tf
+++ terraform/modules/aws_security_group/main.tf
@@ -17,6 +17,16 @@ resource "aws_security_group" "my_sg" {
         }
     }
 
+    ingress {
+
+        description      = "echo request"
+        from_port        = -1
+        to_port          = -1
+        protocol         = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+        
+    }
+
     egress {
     from_port        = 0
     to_port          = 0
